88149bd26f30eb1747cc1025640147865e680a2a
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-17 17:22:23 +0300

    Adding booster and timer for booster

--- Assets/Scripts/DriverController.cs
+++ Assets/Scripts/DriverController.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class DriverController : MonoBehaviour
 {
+    [Header("Driver velocity")]
     [SerializeField] private float rotateSpeed;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float slowSpeed;
     [SerializeField] private float boostSpeed;
+
+    [Header("Timers")]
     [SerializeField] private float timeToWaitBooster;
     [SerializeField] private float timeToDeleteBooster;
+    [SerializeField] private float boosterTime;
 
     private FixedJoystick _fixedJoystick;
     private GameObject _booster;
     private GameObject _newBooster;
+
     private float _rotateAmount;
     private float _moveAmount;
+    private float _moveSpeed;
     private float _waitTime;
-    private bool _isWait = false;
     private bool _isBoosting = false;
+    private bool _isWaiting = true;
+    private float _boosterTime;
 
    
 
     private void Start()
     {
+        _moveSpeed = moveSpeed;
         _fixedJoystick = GameObject.FindGameObjectWithTag("Joystick").GetComponent<FixedJoystick>();
         _booster = GameObject.FindGameObjectWithTag("Boost");
         _waitTime = timeToWaitBooster;
-        
+        _boosterTime = boosterTime;
     }
 
     void Update()
@@ -35,34 +43,31 @@ public class DriverController : MonoBehaviour
         _rotateAmount = _fixedJoystick.Horizontal * rotateSpeed * Time.deltaTime;
         _moveAmount = _fixedJoystick.Vertical * moveSpeed * Time.deltaTime;
 
+        //rotate car
         transform.Rotate(0, 0, -_rotateAmount);
+
+        //Booster work
+        if (_isWaiting && !_isBoosting)
+        {
+            _moveSpeed = moveSpeed;
+            BoosterRespawn();
+        }
+        else if (_isBoosting)
+        {
+            GoWithBooster();
+        }
+
     }
 
-    //private void FixedUpdate()
-    //{
-        /*_rb.velocity = new Vector3(_fixedJoystick.Horizontal * moveSpeed, _fixedJoystick.Vertical * moveSpeed, 0 );
-        if (_fixedJoystick.Horizontal != 0 || _fixedJoystick.Vertical != 0)
-        {
-            /* _horizontal = _fixedJoystick.Horizontal;
-             _rotateAmount = _horizontal * _fixedJoystick.Vertical * rotateSpeed * Time.deltaTime;
-             transform.Rotate(0, 0, -_rotateAmount);*/
-
-           // transform.rotation = Quaternion.Euler(0, 0, angle * _fixedJoystick.Horizontal);
-
-        //}
-
-        //transform.rotation = transform.rotation * Quaternion.AngleAxis(_fixedJoystick.Horizontal * rotateSpeed, Vector3.forward);
-
-        //_rb.AddForce(0, 0, runSpeed * Time.deltaTime);
-   // }
-
+    //Method for drive car with button (MoveController)
     public void Moving()
     {
-        _moveAmount = moveSpeed * Time.deltaTime;
+        _moveAmount = _moveSpeed * Time.deltaTime;
         transform.Translate(0, _moveAmount, 0);
 
     }
 
+    //Method for stop car with button (MoveController)
     public void Stopping()
     {
         transform.Translate(0, 0, 0);
@@ -75,30 +80,42 @@ public class DriverController : MonoBehaviour
 
     IEnumerator BoostEmergence()
     {
-        
         CreateObject();
         yield return new WaitForSeconds(timeToDeleteBooster);
         Destroy(_newBooster);
-    
+        _isWaiting = true;
+
     }
 
+    //Timer for respawn booster
     private void BoosterRespawn()
     {
         _waitTime -= Time.deltaTime;
         if (_waitTime < 0f)
         {
             _waitTime = timeToWaitBooster;
-            _isWait = false;
+            _isWaiting = false;
             StartCoroutine(BoostEmergence());
         }
     }
 
+    //Timer for working booster (how long car go with high speed)
+    private void GoWithBooster()
+    {
+        _boosterTime -= Time.deltaTime;
+        if (_boosterTime < 0f)
+        {
+            _boosterTime = boosterTime;
+            _isBoosting = false;
+            Debug.Log("Boosting finish");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Boost")
         {
-            moveSpeed = boostSpeed;
+            _moveSpeed = boostSpeed;
             _isBoosting = true;
         }
     }
