f28c0651c8b4013a85d2fd8518f38123bb22b15b
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-23 20:27:57 +0300

    Add notification when progress bar is full

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
     [SerializeField] private Slider progressBar;
+    [SerializeField] private GameObject slider;
+    [SerializeField] private GameObject notification;
 
     private DriverController _driver;
-
     private int progress;
-
-    private void Awake()
-    {
-        GameObject driver = GameObject.FindGameObjectWithTag("Driver"); ;
-        _driver = driver.GetComponent<DriverController>();
-    }
 
     private void Start()
     {
-        progress = _driver.GetCountCatch();
+        GameObject driver = GameObject.FindGameObjectWithTag("Driver"); ;
+        _driver = driver.GetComponent<DriverController>();
         progressBar.maxValue = _driver.GetCatchInLevel();
         progressBar.value = 0;
     }
 
     private void Update()
     {
-        progressBar.value = progress;
+        progressBar.value = _driver.GetCountCatch();
+
+        MakeNotification();
+    }
+
+    public void MakeNotification()
+    {
+        if(progressBar.value == progressBar.maxValue)
+        {
+            slider.SetActive(false);
+            notification.SetActive(true);
+        }
     }
 }
